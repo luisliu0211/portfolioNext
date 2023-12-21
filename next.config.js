e37887be83662735b1ad683398852bbe4ea2d746
luis personal <lu0211@hotmail.com>
2023-12-21 19:11:03 +0800

    10

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     domains: ['randomuser.me'],
   },
-  basePath: '/portfolioNext', // 设置成你的 GitHub 仓库名称
-  assetPrefix: '/portfolioNext',
+  // basePath: '/', // 设置成你的 GitHub 仓库名称
+  // assetPrefix: '/',
   exportPathMap: async function () {
     return {
       '/': { page: '/' },
