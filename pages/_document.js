cb3666db0f3c14c9b36bdf3f43e97133cfd5e9bf
luis personal <lu0211@hotmail.com>
2024-01-08 16:51:30 +0800

    uis

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;800;900&family=Roboto:wght@300;400;500;700;900&display=swap"
           rel="stylesheet"
--- pages/posts/test.js
+++ pages/posts/test.js
@@ -4,25 +4,27 @@ import { mdOpt } from '@/lib/markdownConfig';
 import Layout from '@/component/layouts/layout';
 import MarkdownContent from '@/component/layouts/markdownContent';
 import markdownItCheckbox from 'markdown-it-checkbox';
+const apiUrl = process.env.NEXT_PUBLIC_REACT_APP_API_URL;
 export default function Test() {
   const [markdownContent, setMarkdownContent] = useState('');
-  const md = MarkdownIt(mdOpt)
-    .use(markdownItCheckbox)
-    .use(require('markdown-it-highlightjs'));
 
   useEffect(() => {
-    let t = 'http://localhost:8080';
+    const md = MarkdownIt(mdOpt)
+      .use(markdownItCheckbox)
+      .use(require('markdown-it-highlightjs'));
+
+    // let t = 'http://localhost:8080';
     // 在这里发送请求获取 Markdown 内容，你可以使用 fetch 或其他请求库
     fetch(`${apiUrl}/api/posts/markdown`) // 根据你的后端路由来修改
       .then((response) => response.json())
       .then((data) => {
         // 使用 marked 将 Markdown 转换为 HTML
-        console.log(data.markdownContent, 'dd');
+        // console.log(data.markdownContent, 'dd');
         const result = md.render(data.markdownContent);
         setMarkdownContent(result);
       })
       .catch((error) => console.error('Error fetching Markdown:', error));
-    console.log(typeof markdownContent, 'mm');
+    // console.log(typeof markdownContent, 'mm');
   }, [markdownContent]);
 
   return (
